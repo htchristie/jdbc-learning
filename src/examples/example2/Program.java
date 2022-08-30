336ae5b434e1a4ac3b28bf02443f75799443cf82
Cristyane Tamioso <christiehespanha@gmail.com>
2022-08-30 12:13:24 -0300

    returning generated key

--- src/examples/example2/Program.java
+++ src/examples/example2/Program.java
@@ -2,9 +2,7 @@ package examples.example2;
 
 import db.DB;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -22,19 +20,28 @@ public class Program {
                     "INSERT INTO seller "
                     + "(Name, Email, Birthdate, BaseSalary, DepartmentId) "
                     + "VALUES "
-                    + "(?, ?, ?, ?, ?)"
+                    + "(?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
             );
 
-            statement.setString(1, "Carl Purple");
-            statement.setString(2, "carl@gmail.com");
-            statement.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-            statement.setDouble(4, 3000.0);
-            statement.setInt(5, 4);
+            statement.setString(1, "Lisa Jung");
+            statement.setString(2, "lisa@gmail.com");
+            statement.setDate(3, new java.sql.Date(sdf.parse("10/09/1995").getTime()));
+            statement.setDouble(4, 4500.0);
+            statement.setInt(5, 3);
 
             int rowsAffected = statement.executeUpdate();
             //updates data and returns an integer indicating how many lines were altered
 
-            System.out.println("Rows affected: " + rowsAffected);
+            if (rowsAffected > 0) {
+                ResultSet resultSet = statement.getGeneratedKeys();
+                while (resultSet.next()) {
+                    int id = resultSet.getInt(1);
+                    System.out.println("Insert completed. Generated ID: " + id);
+                }
+            } else {
+                System.out.println("No rows affected.");
+            }
         }
         catch (SQLException | ParseException e) {
             e.printStackTrace();
